2ac997b39446b1ebc6eaf63218044fe7d7f5309a
galynary <107271972+galynary@users.noreply.github.com>
2024-02-13 23:42:32 +0200

    change port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 2000 } = process.env;
+const { DB_HOST, PORT = 1234 } = process.env;
 
 mongoose.set("strictQuery", true);
 
