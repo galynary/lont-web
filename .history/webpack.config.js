1cc7fc720446ab4ecf869b3d647c54828c56b217
galynary <107271972+galynary@users.noreply.github.com>
2024-02-16 16:20:51 +0200

    change

--- .history/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js', // Вхідний файл
-  output: {
-    filename: 'bundle.js', // Вихідний файл
-    path: path.resolve(__dirname, 'dist') // Шлях до вихідної директорії
-  }
-};
