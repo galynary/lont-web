7b99c14c90db46ef71c8b5a1661ba143f7e414fc
galynary <107271972+galynary@users.noreply.github.com>
2024-03-10 00:17:44 +0200

    change

---
+++ js/index.js
@@ -0,0 +1,68 @@
+const sendEmailToOwner = require('../backend/helpers/sendEmail');
+const form = document.getElementById("internetForm");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const name = document.getElementById("fullName").value;
+  const address = document.getElementById("address").value;
+  const phone = document.getElementById("phone").value;
+  const tariff = document.getElementById("tariff").value;
+
+  if (validateForm(name, address, phone, tariff)) {
+    // Если все поля заполнены, продолжаем отправку данных
+    const data = {
+      name: fullName,
+      address: address,
+      phone: phone,
+      tariff: tariff
+    };
+
+    const url = "http://localhost:2000/api/register"; // Замените на реальный URL
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+      if (result.success) {
+        // Если данные успешно отправлены на сервер, отправляем письмо на электронную почту владельца компании
+        sendEmailToOwner(data);
+        alert("Заявка успешно отправлена!");
+        // Можно также перенаправить пользователя на страницу "спасибо" или что-то подобное
+      } else {
+        alert("Произошла ошибка при отправке заявки.");
+      }
+    })
+    .catch(error => {
+      console.error("Ошибка отправки заявки:", error);
+      alert("Произошла ошибка при отправке заявки.");
+    });
+  }
+}
+
+function validateForm(name, address, phone, tariff) {
+  if (!name) {
+    alert("Заполните поле 'Имя'");
+    return false;
+  }
+  if (!address) {
+    alert("Заполните поле 'Адрес'");
+    return false;
+  }
+  if (!phone) {
+    alert("Заполните поле 'Телефон'");
+    return false;
+  }
+  if (!tariff) {
+    alert("Заполните поле 'Тариф'");
+    return false;
+  }
+  return true; // Все поля заполнены
+}
