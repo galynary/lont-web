e851938881a4f29f77dbdab142d74461d57a0806
galynary <107271972+galynary@users.noreply.github.com>
2024-05-07 09:45:17 +0300

    change

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,20 @@
-(() => {
-	const mobileMenu = document.querySelector(".js-menu-container");
-	const openMenuBtn = document.querySelector(".js-open-menu");
-	const closeMenuBtn = document.querySelector(".js-close-menu");
+// Select elements
+const mobileMenu = document.querySelector('.menu__container');
+const openMenuBtn = document.querySelector('.js-open-menu');
+const closeMenuBtn = document.querySelector('.js-close-menu');
 
-	openMenuBtn.addEventListener("click", toggleMenu);
-	closeMenuBtn.addEventListener("click", () => {
-		toggleMenu();
-		window.scrollTo({
-			top: 0
-		});
-	});
+// Function to open the menu
+function openMenu() {
+  mobileMenu.classList.remove('js-menu-container'); // Remove the class to display the menu
+}
 
-	function toggleMenu() {
-		mobileMenu.classList.toggle("js-menu-container");
-	}
-})();
+// Function to close the menu
+function closeMenu() {
+  mobileMenu.classList.add('js-menu-container'); // Add the class back to hide the menu
+}
+
+// Event listener for opening the menu when clicking openMenuBtn
+openMenuBtn.addEventListener('click', openMenu);
+
+// Event listener for closing the menu when clicking closeMenuBtn
+closeMenuBtn.addEventListener('click', closeMenu);
