b2f7d208dc5de63b48e0cc64ec0ab5e4ffa90105
galynary <107271972+galynary@users.noreply.github.com>
2024-05-09 20:27:38 +0300

    CHANGE

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,18 @@ let currentSlideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let slides = document.getElementsByClassName("slide");
+  let slides = document.getElementsByClassName('slide');
 
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
 
   if (currentSlideIndex >= slides.length) {
     currentSlideIndex = 0;
   }
 
-  slides[currentSlideIndex].style.display = "block";
+  slides[currentSlideIndex].style.display = 'block';
   currentSlideIndex++;
 
-  setTimeout(showSlides, 7000); // Інтервал перемикання слайдів у мілісекундах (в даному випадку - 2000 мс або 2 секунди)
+  setTimeout(showSlides, 9000); // Інтервал перемикання слайдів у мілісекундах (в даному випадку - 2000 мс або 2 секунди)
 }
